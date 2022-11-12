753ae71d66070dd31590f526aaa1fccb2444b949
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-12 13:34:04 +0530

    Just as YouTuber does: Example with Async Await

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,33 +1,39 @@
 /* Just as YouTuber does */
-// First seeing the example without async await
+// Using async await await in the example
 console.log('person1: shows ticket');
 console.log('person2: shows ticket');
 
-const promiseWifeBringingTicks = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('ticket');
-    }, 3000);
-});
+const preMovie = async () => {
+    const promiseWifeBringingTicks = new Promise((resolve, reject) => { setTimeout(() => { resolve('ticket'); }, 3000); });
 
-const getPopcorn = promiseWifeBringingTicks.then((t) => {
-    console.log('wife: i have the tickets');
+    const getPopcorn = new Promise((resolve, reject) => { resolve('popcorn'); });
+
+    const addButter = new Promise((resolve, reject) => { resolve('butter'); });
+
+    let ticket = await promiseWifeBringingTicks;
+
+    console.log(`wife: i have the ${ticket}`);
     console.log(`husband: we should go in`);
     console.log('wife: no i am hungry');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} popcorn`);
-    });
-});
 
-const getButter = getPopcorn.then((t) => {
-    console.log(`husband: i got some popcorn`);
+    let popcorn = await getPopcorn;
+
+    console.log(`husband: i got some ${popcorn}`);
     console.log(`husband: we should go in`);
     console.log('wife: I need butter on popcorn');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} butter`);
-    });
-});
 
-getButter.then((t) => { console.log(t); })
+    let butter = await addButter;
+
+    console.log(`husband: i got some ${butter} on popcorn`);
+    console.log(`wife: lets go`);
+
+    return ticket;
+};
+
+preMovie().then((msg) => console.log(`person3: shows ${msg}`));
 
 console.log('person4: shows ticket');
 console.log('person5: shows ticket');
+
+// NOTE: Async function always returns a promise
+// NOTE: Await can be used only inside an async function
