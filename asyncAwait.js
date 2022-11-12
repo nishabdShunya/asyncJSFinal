588534bef6a3eaf8d4260f3e135c195991d65368
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-12 18:26:23 +0530

    asyncAwait sharpener additional task three

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,20 +1,69 @@
-/* Just as YouTuber does: Async-Await & Error Handling */
-console.log('person1: shows ticket');
-console.log('person2: shows ticket');
+/* Additional Sharpener Task Three
+Convert the createPost, deletePost you wrote previously into async await completely. */
+let posts = [
+    { title: 'Post One', body: 'This is Post One' },
+    { title: 'Post Two', body: 'This is Post Two' }
+];
 
-const preMovie = async () => {
-    const promiseWifeBringingTicks = new Promise((resolve, reject) => { setTimeout(() => { reject('ticket'); }, 3000); });
-    let ticket;
+function getPost() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post) => {
+            output += `<li>${post.title}</li>`;
+        })
+        document.querySelector('main').innerHTML = output;
+    }, 1000)
+}
+
+async function createPost(post) {
+    const postCreatePromise = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            const error = false;
+            if (!error) {
+                resolve('Promise Resolved');
+            }
+            else {
+                reject('Error:Something went wrong');
+            }
+        }, 2000);
+    });
     try {
-        ticket = await promiseWifeBringingTicks;
+        getPost();
     }
-    catch (err) {
-        ticket = 'sad face'
+    catch (error) {
+        console.log(error);
     }
-    return ticket;
 };
 
-preMovie().then((msg) => console.log(`person3: shows ${msg}`));
+async function deletePost() {
+    const postDeletePromise = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (posts.length != 0) {
+                resolve(posts.pop());
+            }
+            else {
+                reject('Array is Empty Now');
+            }
+        }, 1000);
+    });
+    try {
+        console.log('This is the deleted post: ', postDeletePromise);
+        getPost();
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
 
-console.log('person4: shows ticket');
-console.log('person5: shows ticket');
+createPost({ title: 'Post Three', body: 'This is Post Three' })
+    .then(deletePost).catch(err => console.log(err))
+    .then(deletePost).catch(err => console.log(err))
+    .then(deletePost).catch(err => console.log(err))
+    .then(deletePost).catch(err => console.log(err))
+    .then(() => {
+        createPost({ title: 'Post Four', body: 'This is Post Four' })
+            .then(deletePost).catch(err => console.log(err))
+            .then(deletePost).catch(err => console.log(err))
+    })
+    .catch(err => console.log(err));
