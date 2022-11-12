6552fc6c6623252810be7b7b3be40c1e09be21a5
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-12 11:36:32 +0530

    promise.all as YouTuber does

---
+++ promiseAll.js
@@ -0,0 +1,11 @@
+/* Just as YouTuber does */
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'GoodBye');
+})
+
+Promise.all([promise1, promise2, promise3])
+    .then((values) => {
+        console.log(values);
+    })
