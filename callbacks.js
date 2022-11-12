ab4223341d9b9a7ec32b75b32833d5345cd089a7
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-12 10:11:57 +0530

    callbacks sharpener additional task three

--- callbacks.js
+++ callbacks.js
@@ -1,16 +1,13 @@
-/* Additional Sharpener Task Two
-Lets also record when the post was created. So now onwards whenever. a new post is created  add a new key called createdAt in each post. CreatedAt should have the timestamp of  when the post was created. So now your post object would look like { title, body, createdAt}
-On the screen show the user how long back each of the post was created in seconds ago. Just add "{ current timestamp  - postcreated At timestamp }" on each of the post. It should look like the following:
-Post 1 created x seconds ago
-Post 2 created y seconds ago
-Post 3 created z seconds ago */
+/* Additional Sharpener Task Three
+Update the value of timestamp every second. */
 let posts = [
     { title: 'Post One', body: 'This is Post One', createdAt: Math.floor((new Date().getTime()) / 1000) },
     { title: 'Post Two', body: 'This is Post Two', createdAt: Math.floor((new Date().getTime()) / 1000) }
 ];
-
+let intervalID;
 function getPost() {
-    setTimeout(() => {
+    clearInterval(intervalID);
+    intervalID = setInterval(() => {
         let output = '';
         posts.forEach((post) => {
             output += `<li>${post.title} | created ${Math.floor((new Date().getTime()) / 1000) - post.createdAt} seconds ago</li>`;
