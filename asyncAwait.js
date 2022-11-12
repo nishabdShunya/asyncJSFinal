314491079529d56901ed2d67d21d3ca2f00cf7ec
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-12 13:45:07 +0530

    asyncAwait sharpener additional task one

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,39 +1,39 @@
-/* Just as YouTuber does */
-// Using async await await in the example
+/* Additional Sharpener Task One
+Create a new promise called getColdDrinks which come after husband gets butter. Integrate the code in promises' code */
 console.log('person1: shows ticket');
 console.log('person2: shows ticket');
 
-const preMovie = async () => {
-    const promiseWifeBringingTicks = new Promise((resolve, reject) => { setTimeout(() => { resolve('ticket'); }, 3000); });
+const promiseWifeBringingTicks = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('ticket');
+    }, 3000);
+});
 
-    const getPopcorn = new Promise((resolve, reject) => { resolve('popcorn'); });
-
-    const addButter = new Promise((resolve, reject) => { resolve('butter'); });
-
-    let ticket = await promiseWifeBringingTicks;
-
-    console.log(`wife: i have the ${ticket}`);
+const getPopcorn = promiseWifeBringingTicks.then((t) => {
+    console.log('wife: i have the tickets');
     console.log(`husband: we should go in`);
     console.log('wife: no i am hungry');
+    return new Promise((resolve, reject) => {
+        resolve(`${t} popcorn`);
+    });
+});
 
-    let popcorn = await getPopcorn;
-
-    console.log(`husband: i got some ${popcorn}`);
+const getButter = getPopcorn.then((t) => {
+    console.log(`husband: i got some popcorn`);
     console.log(`husband: we should go in`);
     console.log('wife: I need butter on popcorn');
+    return new Promise((resolve, reject) => {
+        resolve(`${t} butter`);
+    });
+});
 
-    let butter = await addButter;
+const getColdDrink = getButter.then((t) => {
+    return new Promise((resolve, reject) => {
+        resolve(`${t} cold-drink`);
+    });
+})
 
-    console.log(`husband: i got some ${butter} on popcorn`);
-    console.log(`wife: lets go`);
-
-    return ticket;
-};
-
-preMovie().then((msg) => console.log(`person3: shows ${msg}`));
+getColdDrink.then((t) => console.log(t));
 
 console.log('person4: shows ticket');
 console.log('person5: shows ticket');
-
-// NOTE: Async function always returns a promise
-// NOTE: Await can be used only inside an async function
