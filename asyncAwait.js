dfad20b9397678cbecbb6703eb70aed41f09c4c6
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-12 13:52:13 +0530

    asyncAwait sharpener additional task two

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,39 +1,43 @@
-/* Additional Sharpener Task One
-Create a new promise called getColdDrinks which come after husband gets butter. Integrate the code in promises' code */
+/* Additional Sharpener Task Two
+Create a new promise called getColdDrinks which come after husband gets butter. Integrate the code in asyncAwait code. */
 console.log('person1: shows ticket');
 console.log('person2: shows ticket');
 
-const promiseWifeBringingTicks = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('ticket');
-    }, 3000);
-});
+const preMovie = async () => {
+    const promiseWifeBringingTicks = new Promise((resolve, reject) => { setTimeout(() => { resolve('ticket'); }, 3000); });
 
-const getPopcorn = promiseWifeBringingTicks.then((t) => {
-    console.log('wife: i have the tickets');
+    const getPopcorn = new Promise((resolve, reject) => { resolve('popcorn'); });
+
+    const addButter = new Promise((resolve, reject) => { resolve('butter'); });
+
+    const getColdDrinks = new Promise((resolve, reject) => { resolve('cold-drinks') });
+
+    let ticket = await promiseWifeBringingTicks;
+
+    console.log(`wife: i have the ${ticket}`);
     console.log(`husband: we should go in`);
     console.log('wife: no i am hungry');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} popcorn`);
-    });
-});
 
-const getButter = getPopcorn.then((t) => {
-    console.log(`husband: i got some popcorn`);
+    let popcorn = await getPopcorn;
+
+    console.log(`husband: i got some ${popcorn}`);
     console.log(`husband: we should go in`);
     console.log('wife: I need butter on popcorn');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} butter`);
-    });
-});
 
-const getColdDrink = getButter.then((t) => {
-    return new Promise((resolve, reject) => {
-        resolve(`${t} cold-drink`);
-    });
-})
+    let butter = await addButter;
 
-getColdDrink.then((t) => console.log(t));
+    console.log(`husband: i got some ${butter} on popcorn`);
+    console.log(`wife: lets go`);
+
+    let coldDrinks = await getColdDrinks;
+
+    console.log(`husband: wait! i have also got some ${coldDrinks}, grab them`);
+    console.log(`wife: yeah sure! thanks darling`);
+
+    return ticket;
+};
+
+preMovie().then((msg) => console.log(`person3: shows ${msg}`));
 
 console.log('person4: shows ticket');
 console.log('person5: shows ticket');
